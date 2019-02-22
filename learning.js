a344393cb9e1bc82bd8a4eb0832f522b93c726d6
MHK <mhk@Majids-MacBook-Pro.local>
2019-02-22 22:58:20 +0000

    Update

--- learning.js
+++ learning.js
@@ -20,23 +20,23 @@ function BubbleSort(arr) {
     while (swap)
     return sortedArray;
 }
+console.log('\n[Bubble Sort]');
 console.log(BubbleSort([15, 2, 4, 8, 6, 12, 5, 11, 3, 1, 10]));
 
 function InsertionSort(arr) {
-    var A = Array.from(arr);
+    var sortedArray = Array.from(arr)
     var i, j, key
-    for (j = 1; j < A.length; j++) {
-        key = A[j];
+    for (j = 1; j < sortedArray.length; j++) {
+        key = sortedArray[j];
         i = j - 1;
-        while (i > -1 && A[i] > key) {
-            A[i + 1] = A[i];
+        while (i > -1 && sortedArray[i] > key) {
+            sortedArray[i + 1] = sortedArray[i];
             i = i - 1;
         }
-        A[i + 1] = key;
+        sortedArray[i + 1] = key;
     }
-    return A;
+    return sortedArray;
 }
-
-console.log(insertionSort([5, 2, 4, 6, 1, 3]));
-var result = InsertionSort([5, 2, 4, 3, 1, 6])
+console.log('\n[Insert Sort]');
+var result = InsertionSort([15, 2, 4, 8, 6, 12, 5, 11, 3, 1, 10])
 console.log(result);
